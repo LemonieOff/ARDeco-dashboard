e8bde601c514e3aeb5dbf270ab864869243e6af2
louis <louis.villedieu@epitech.eu>
2024-01-21 09:18:44 -0500

    fix jwt issues

--- monitoring.js
+++ monitoring.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors'); // Require the cors package
 
 // Port sur lequel le serveur sera écouté
-const port = 3000;
+const port = 3001;
 
 // Servir des fichiers statiques (votre HTML, CSS, JS, etc.)
 app.use(express.static(path.join(__dirname, 'public')));
@@ -21,6 +22,20 @@ app.get('/ticket/:ticketId', (req, res) => {
     const ticketId = req.params.ticketId;
     res.sendFile(path.join(__dirname, 'public/ticketChat.html'));
 });
+
+const corsOptions = {
+    origin: [
+        "http://localhost:3000",
+        "http://localhost:3001",
+        "https://ardeco.app",
+        "https://api.ardeco.app",
+        "https://support.ardeco.app",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"]
+};
+
+app.use(cors(corsOptions));
 
 // Démarrer le serveur
 app.listen(port, () => {
